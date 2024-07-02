bb75f5e6e1b1822bf9df4a380e3c1b23d7bcc9eb
Gatzmoz <gilangminecraft21@gmail.com>
2024-07-02 12:46:17 +0700

    feat: adding admin page

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: 'jit',
-	content: ['./*.html'],
+	content: ['./*.html', './**/*.html'],
 	theme: {
 		extend: {
 			colors: {
 				'gasendra-blue-primary': '#289fc3',
 				'gasendra-blue-secondary': '#9fc8b7',
 				primary: '#edbc48',
+				'cms-gray': '#454D55',
+				'cms-red': '#e34b3c',
+				'cms-light-gray': '#d9d9d9',
+				'cms-dark-gray': '#343a40',
+				'cms-darker-gray': '#2F3439',
+				'cms-text-gray': '#6C757D',
+				'cms-blue': '#3f6791',
 			},
 		},
 	},
